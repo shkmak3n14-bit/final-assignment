9a36da47a9a4e24f4bdfcea55ec81412edfc385a
akio.seino <shkma.k3.n14@gmail.com>
2026-01-31 21:17:27 +0900

    Question No.5-1

--- constants.py
+++ constants.py
@@ -52,7 +52,8 @@ RAG_CHUNK_OVERLAP = 80
 SUPPORTED_EXTENSIONS = {
     ".pdf": PyMuPDFLoader,
     ".docx": Docx2txtLoader,
-    ".csv": lambda path: CSVLoader(path, encoding="utf-8")
+    ".csv": lambda path: CSVLoader(path, encoding="utf-8"),
+    ".txt": lambda path: TextLoader(path, encoding="utf-8")
 }
 WEB_URL_LOAD_TARGETS = [
     "https://generative-ai.web-camp.io/"
